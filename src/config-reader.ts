c47bd1364690a4ecb4c76cbdaa48c0940218f812
Naveen Anto Alangararaj <nalangar@linkedin.biz>
2020-04-14 18:45:24 -0700

    add toolkit

--- src/config-reader.ts
+++ src/config-reader.ts
@@ -1,3 +1,5 @@
+import { GitHub, context } from '@actions/github';
+import { Toolkit } from 'actions-toolkit';
 
 export class ConfigReader {
 
@@ -8,7 +10,10 @@ export class ConfigReader {
     }
 
     public readFromWorkspace() {
+        let tk = new Toolkit({token: this.token});
+        var fileContent = tk.getFile('README.md');
         console.log("Reading with token :", this.token, " from workspace : ", process.env.GITHUB_WORKSPACE);
+        console.log('Read file content: ', fileContent);
     }
 
 }
